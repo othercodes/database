9d69918100e20899ae53d88bf606c5f6ded0efa8
David Unay Santisteban <slavepens@gmail.com>
2014-03-10 08:37:38 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,26 @@
+include 'configuration.php';
+inlcude 'database.pdo.class.php';
+
+// get a instance
+$db = database::getInstance();
+
+// simple query, list of objects
+$sql = "SELECT * FROM usuarios";
+$db->query($sql);
+$usuarios = $db->loadObjectList();
+var_dump($usuarios);
+
+// simple query JSON List
+$sql = "SELECT * FROM usuarios";
+$db->query($sql);
+$usuario = $db->loadJsonObjectList();
+var_dump($usuario);
+
+// query with parameters, one object
+$sql = "SELECT * FROM usuarios WHERE id = :id";
+$params = array(
+    ':id' => 1
+);
+$db->query($sql,$params);
+$usuario = $db->loadObject();
+var_dump($usuario);
