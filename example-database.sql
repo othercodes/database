ef93106c5553ad1f3f540ddc749e385fa4bf1e53
David Unay Santisteban <slavepens@gmail.com>
2014-05-27 08:53:51 +0200

    Update example-database.sql

--- example-database.sql
+++ example-database.sql
@@ -2,9 +2,13 @@
 CREATE DATABASE test;
 USE test;
 
-CREATE TABLE `users` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT NULL,
-  `surname` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE ts_users (
+  id int(5) NOT NULL AUTO_INCREMENT,
+  name varchar(20) DEFAULT NULL,
+  surname varchar(30) DEFAULT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB;
+
+INSERT INTO ts_users VALUES
+  (1,'Walter','White'),
+  (2,'Sheldon','Cooper');
